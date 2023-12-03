9e16fac8be3c4c1d0bfae9d8e72d8069d56d2912
IamMrTeapot <87607928+IamMrTeapot@users.noreply.github.com>
2023-12-03 18:52:22 +0700

    Nested Struct and pointer

--- structs/main.go
+++ structs/main.go
@@ -2,9 +2,15 @@ package main
 
 import "fmt"
 
+type contactInfo struct {
+	email   string
+	zipCode int
+}
+
 type person struct {
 	firstName string
 	lastName  string
+	contactInfo
 }
 
 func main() {
@@ -12,9 +18,24 @@ func main() {
 	//Zero Value initialization
 	//var alex person
 
-	alex := person{firstName: "Alex", lastName: "Anderson"}
-	alex.firstName = "Alexis"
+	jim := person{
+		firstName: "Jim",
+		lastName:  "Party",
+		contactInfo: contactInfo{
+			email:   "jim@gmail.com",
+			zipCode: 94000,
+		},
+	}
 
-	fmt.Println(alex)
-	fmt.Printf("%+v", alex)
+	jim.updateName("Jimmy")
+	jim.print()
+
+}
+
+func (p *person) updateName(newFirstName string) {
+	(*p).firstName = newFirstName
+}
+
+func (p person) print() {
+	fmt.Printf("%+v", p)
 }
